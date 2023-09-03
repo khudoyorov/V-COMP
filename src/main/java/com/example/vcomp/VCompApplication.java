06ecc85bf280de89601a75c15e11a44cda7f6442
bobur <boburbekkhudoyorov@gmail.com>
2023-09-03 23:04:10 +0500

    first commit

---
+++ src/main/java/com/example/vcomp/VCompApplication.java
@@ -0,0 +1,13 @@
+package com.example.vcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VCompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vcomp/VCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vcomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
